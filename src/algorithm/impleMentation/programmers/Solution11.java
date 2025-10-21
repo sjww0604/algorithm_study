18487a7cdf4e9fbe97b6f20b971b6c05eb8d12cd
sjww0306 <sjww03@gmail.com>
2025-10-22 08:45:09 +0900

    refactor : 클래스명 오타 수정

--- src/algorithm/impleMentation/programmers/Solution11.java
+++ src/algorithm/impleMentation/programmers/Solution11.java
@@ -1,6 +1,6 @@
 package algorithm.impleMentation.programmers;
 
-public class Soultion11 {
+public class Solution11 {
     public String solution(int num) {
         String answer = "";
         if (num % 2 == 0) {
