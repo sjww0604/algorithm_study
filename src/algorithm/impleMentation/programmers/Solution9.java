cad24e7ca36c8fe7b12c2bba708fba4ac61f338d
sjww0306 <sjww03@gmail.com>
2025-10-21 09:31:58 +0900

    feat : 프로그래머스 짝수의 합 알고리즘 문제풀이 진행

---
+++ src/algorithm/impleMentation/programmers/Solution9.java
@@ -0,0 +1,13 @@
+package algorithm.impleMentation.programmers;
+
+// 정수 n이 주어질 때, n이하의 짝수를 모두 더한 값을 return 하도록 solution 함수를 작성해주세요.
+public class Solution9 {
+        public int solution(int n) {
+            int answer = 0;
+
+            for (int i = 2; i <= n; i += 2) {
+                answer += i;
+            }
+            return answer;
+        }
+    }
