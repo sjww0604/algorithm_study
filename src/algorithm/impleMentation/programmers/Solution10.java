f62f615cf0627b06b7aebfcce262d643b3d96704
sjww0306 <sjww03@gmail.com>
2025-10-21 09:39:13 +0900

    feat : 프로그래머스 배열의 평균값 알고리즘 문제풀이 진행

---
+++ src/algorithm/impleMentation/programmers/Solution10.java
@@ -0,0 +1,12 @@
+package algorithm.impleMentation.programmers;
+
+public class Solution10 {
+        public double solution(int[] numbers) {
+            double answer = 0;
+            for (int i = 0; i < numbers.length; i++) {
+                answer += numbers[i];
+            }
+            answer = answer / numbers.length;
+            return answer;
+        }
+    }
