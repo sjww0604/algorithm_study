6d2657de004052b47951c2888caea60a4f0cf52d
sjww0306 <sjww03@gmail.com>
2025-10-21 09:11:01 +0900

    feat : 프로그래머스 두 수의 나눗셈 알고리즘 문제풀이 진행

---
+++ src/algorithm/impleMentation/programmers/Solution7.java
@@ -0,0 +1,12 @@
+package algorithm.impleMentation.programmers;
+
+// 정수 num1과 num2가 매개변수로 주어질 때, num1을 num2로 나눈 값에 1,000을 곱한 후 정수 부분을 return 하도록 solution 함수를 완성해주세요.
+public class Solution7 {
+    class Solution {
+        public int solution(int num1, int num2) {
+            double answer = 0;
+            answer = (double) num1 / (double) num2 * 1000; //나눗셈 결과를 Double로 받아 * 1000 연산 시 정수부분이 출력되도록 구현
+            return (int) answer;
+        }
+    }
+}
