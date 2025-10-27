3fceee70cd760eaca148d61b41aac2a6831c68c0
sjww0306 <sjww03@gmail.com>
2025-10-27 09:34:52 +0900

    feat : 프로그래머스 내적 알고리즘 문제풀이

---
+++ src/algorithm/impleMentation/programmers/Solution32.java
@@ -0,0 +1,13 @@
+package algorithm.impleMentation.programmers;
+
+public class Solution32 {
+    class Solution {
+        public int solution(int[] a, int[] b) {
+            int answer = 0;
+            for (int i = a.length - 1; i >= 0; i--) {
+                answer += a[i] * b[i];
+            }
+            return answer;
+        }
+    }
+}
