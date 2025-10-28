c4975369604c214d478b87c34861de68eb4d03af
sjww0306 <sjww03@gmail.com>
2025-10-28 09:50:59 +0900

    feat : 프로그래머스 최대공약수와 최소공배수 알고리즘 문제풀이

---
+++ src/algorithm/impleMentation/programmers/Solution39.java
@@ -0,0 +1,26 @@
+package algorithm.impleMentation.programmers;
+
+public class Solution39 {
+    class Solution {
+        public int[] solution(int n, int m) {
+            int[] answer = {};
+            if (n == 0 && m == 0) {
+                throw new IllegalArgumentException("0과 0의 최대공약수는 정의되지 않습니다.");
+            }
+
+            int gcd = getGcd(n, m);
+            int lcm = n *  m / gcd;
+
+            return answer = new int[]{gcd, lcm};
+        }
+
+        private int getGcd(int a, int b) {
+            while (b != 0) {
+                int temp = a % b;
+                a = b;
+                b = temp;
+            }
+            return a;
+        }
+    }
+}
